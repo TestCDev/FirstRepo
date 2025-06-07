ec4fab2e8daa53cbf54bce94755285050f8ca0fd
TestCDev <office.use.learn.25@gmail.com>
2025-06-07 13:15:52 +0530

    Update pythonPractice.py

--- pythonPractice.py
+++ pythonPractice.py
@@ -56,3 +56,51 @@ print(f"Slice operator method. New list from this method :\n{lis4}\n")
 # output :- Slice operator method. New list from this method :
 #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+# to join two list ------------------------------------------------------------------------------------------------------------------------------------------------------------
+lis1 = list(range(1,5))
+lis2 = list(range(5,11))
+print(f"The original list 1: {lis1}")
+print(f"The original list 2: {lis2}")
+# output : The original list 1: [1, 2, 3, 4]
+# The original list 2: [5, 6, 7, 8, 9, 10]
+
+
+# 1. + operator
+lis3 = lis1+lis2
+print(f"Using + operator : {lis3}")
+# output : Using + operator : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+# 2. append() method
+for x in lis2:
+    lis1.append(x)
+print(f"Using append method : {lis1}")
+# output : Using append method : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+
+# 3. extend() method
+#lis4 = lis1.extend(lis2) # output = None
+lis1.extend(lis2)
+print(f"Using extend method : {lis1}")
+# output :Using extend method : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 6, 7, 8, 9, 10]
+
+
+# 4. list comprehension
+lis4 = [item for sublis in [lis1,lis2] for item in sublis]
+print(f"Using list comprehension : {lis4}")
+# output :Using list comprehension : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 6, 7, 8, 9, 10, 5, 6, 7, 8, 9, 10]
+
+# Index -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# syntax : list.index(elmnt, start, end)
+# only takes one element of the list as argument
+fruit = ['apple', 'banana', 'cherry']
+
+x = fruit.index("cherry")
+print(x)    # it returns the index of the element specified in the provided list
+# output  : 2
+
+fruits = ['apple', 'banana', 'cherry', 'kiwi', 'mango', 'orange', 'cherry']
+y = fruits.index("cherry", 4)
+print(y) # here search begins from the index 4
+# output  : 6
